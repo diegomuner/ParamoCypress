91c199767f8004a00cc855c9bc0071ef467e1b3e
diegomuner <diegomuner@hotmail.com>
2023-03-05 18:11:43 -0300

    last commit

--- cypress/e2e/Tests/SignUpTest/SignUp.spec.js
+++ cypress/e2e/Tests/SignUpTest/SignUp.spec.js
@@ -5,17 +5,15 @@ Given("I navigate to demo casino homepage", () => {
     cy.visit("/")
 });
 
-
 When("I see and close the welcome modal", () => {
 
-cy.closeWelcomeModal();
+  cy.closeWelcomeModal();
 
 });
-
-  
+ 
 When("User clicks sign up and populates sign up form", () => {  
 
-    cy.signUp();
+  cy.signUp();
     
 });
 
